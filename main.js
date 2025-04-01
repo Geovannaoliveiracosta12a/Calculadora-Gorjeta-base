7d1d26761101c7ff458fd50e4e2edb2b1c5d1464
geovannaoliveiracosta10 <geovannaoliveiracosta10@gmail.com>
2025-04-01 01:11:15 +0000

    função para calcular a gorjeta e o total finalizada

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 }
 
 function receiveNumberOfPeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
 function receiveTipPercentageValue(value){
@@ -16,8 +18,41 @@ function receiveTipPercentageValue(value){
         buttonSelected.classList.remove("button-selected")
     }
 
-    tipPercentage = value / 100
+    let customTipInput = document.querySelector("#custom-tip").valueAsNumber
+    console.log(customTipInput)
 
+    if(customTipInput.value !== ""){
+        customTipInput.value = ""
+    }
+
+    tipPercentage = value / 100
+    console.log(tipPercentage)
+    calculate()
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+    calculate()
+}
+
+function receiveCustomTipPercentageValue(){
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+    calculate()
+}
+
+function calculate(){
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+        let amountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        amountStrong.innerText = `$0${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = bill / numberOfPeople + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    }else{
+        console.log("Preencha Tudo")
+    }
 }
