f5333332dd8c759263e808f9a61b33091e9ae5a3
geovannaoliveiracosta10 <geovannaoliveiracosta10@gmail.com>
2025-04-01 01:41:11 +0000

    função para resetar as informações da página e finalização do projeto

--- main.js
+++ main.js
@@ -39,4 +39,46 @@ function receiveCustomTipPercentageValue(){
     calculate()
 }
 
+function calculate(){
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+        let amountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        amountStrong.innerText = `$0${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = bill / numberOfPeople + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    }else{
+        console.log("Preencha Tudo")
+    }
+}
+
+function reset(){
+    bill = 0
+    document.querySelector("#bill").value = ""
+
+    numberOfPeople = 0
+    document.querySelector("#people").value = ""
+
+    tipPercentage = 0
+    removeButtonSelectedClass()
+
+   
+    let customTipInput = document.querySelector("#custom-tip").valueAsNumber
+    console.log(customTipInput)
+
+    if(customTipInput.value !== ""){
+        customTipInput.value = ""
+    }
+    document.querySelector(".amount strong").innerText = "$0.00"
+
+    document.querySelector(".total strong").innerText = "$0.00"
+    
+}
+
+function removeButtonSelectedClass(){
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+}
     
